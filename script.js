f17eac7cfce2f97fd11de81de5a44e2e3f55c6cb
mdwiltfong <wiltfong.michael1@gmail.com>
2021-07-31 16:01:00 -0400

    clicked tasks have a strike in them

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let form = document.querySelector("form");
-let unorderedList=document.querySelector("ul");
-form.addEventListener('submit',(e)=>{
+let unorderedList = document.querySelector("ul");
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     //console.log(`Form submited`);
-    let input=form.elements[0].value;
-    let li =document.createElement("li");
-    li.innerText=input;
+    let input = form.elements[0].value;
+    let li = document.createElement("li");
+    li.innerText = input;
     unorderedList.append(li);
+})
+
+unorderedList.addEventListener('click', (e) => {
+    let target=e.target;   
+    target.style.textDecoration = 'line-through';
+    let p = document.createElement('p');
+    p.innerText="x";
+    target.append(p);
+
 
 })
