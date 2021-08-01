dfcb0423035010da1af715c9abb6f016f035070d
mdwiltfong <wiltfong.michael1@gmail.com>
2021-08-01 03:08:06 -0400

    removed comments

--- script.js
+++ script.js
@@ -5,8 +5,8 @@ let inputText = document.querySelector("input[type=text]")
 //remove task
 form.addEventListener('change', (e) => {
     e.preventDefault();
-    console.log(`Change`);
-    console.log(e.target);
+    //console.log(`Change`);
+    //console.log(e.target);
     if (e.target.getAttribute("type") !== `text`) {
         let label = e.path[1];
         form.removeChild(label);
@@ -17,8 +17,8 @@ form.addEventListener('change', (e) => {
 //Add task
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(`input`)
-    console.log(typeof e.path[0][0].value)
+  //  console.log(`input`)
+    //console.log(typeof e.path[0][0].value)
     let newLabel = document.createElement('label');
     let newInput = document.createElement('input');
     newInput.setAttribute('type', 'checkbox');
