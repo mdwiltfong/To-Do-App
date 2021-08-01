3a90f37b82fe2cc627ff99f4d386b75081124e97
mdwiltfong <wiltfong.michael1@gmail.com>
2021-08-01 02:44:48 -0400

    Users can add events

--- script.js
+++ script.js
@@ -1,20 +1,28 @@
-let form = document.querySelector("form");
-let unorderedList = document.querySelector("ul");
-form.addEventListener('submit', (e) => {
+let checkbox=document.querySelector("input[type=checkbox]")
+let form=document.querySelector("form");
+let inputText=document.querySelector("input[type=text]")
+
+
+checkbox.addEventListener('change', (e) => {
     e.preventDefault();
-    //console.log(`Form submited`);
-    let input = form.elements[0].value;
-    let li = document.createElement("li");
-    li.innerText = input;
-    unorderedList.append(li);
+    console.log(`Change`);
+    console.log(e.path[1]);
+
+    let label=e.path[1];
+    form.removeChild(label);
+   
 })
-
-unorderedList.addEventListener('click', (e) => {
-    let target=e.target;   
-    target.style.textDecoration = 'line-through';
-    let p = document.createElement('p');
-    p.innerText="x";
-    target.append(p);
-
-
+//Add task
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    console.log(`input`)
+    console.log(typeof e.path[0][0].value)
+   let newLabel = document.createElement('label');
+   let newInput = document.createElement('input');
+   newInput.setAttribute('type','checkbox');
+   newLabel.append(newInput);
+   let text=document.createTextNode(`${e.path[0][0].value}`);
+   newLabel.append(text);
+   form.append(newLabel);
+    inputText.value=" ";
 })
